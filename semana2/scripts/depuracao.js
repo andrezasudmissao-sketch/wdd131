57c80bcc6b921da75a0336344ed202d17c7c07d3
andrezasudmissao <andreza.sudmissao@gmail.com>
2026-07-08 19:18:57 -0300

    js devtools

---
+++ semana2/scripts/depuracao.js
@@ -0,0 +1,16 @@
+
+const resultadoRaio = document.getElementById('raio');
+const resultadoArea = document.getElementById('area');
+
+let area = 0;
+const PI = 3.14159;
+
+let raio = 10;
+area = PI * raio * raio;
+resultadoRaio.textContent = raio;
+resultadoArea.textContent = area;
+
+raio = 20;
+area = PI * raio * raio;
+resultadoRaio.innerHTML = `<strong>${raio}</strong>`;
+resultadoArea.textContent = `<strong>${area}</strong>`;
